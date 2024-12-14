efd0de80636e8744ce5f0fd98620230290d0cf9c
Tran T. Ngoc <ttngoc653@gmail.com>
2024-12-15 02:19:51 +0700

    add copy right for footer

--- src/frontend/components/footer/Footer.jsx
+++ src/frontend/components/footer/Footer.jsx
@@ -1,7 +1,16 @@
 import "./footer.css";
+import packageJson from "../../../../package.json";
 
 function Footer() {
-  return <footer>this is footer</footer>;
+  return (
+    <footer>
+      <span>Copyright 2024 by Zymuk Tran. All Rights Reserved.</span>
+      {packageJson.datetimedeploy !== undefined &&
+        packageJson.datetimedeploy.length > 0 && (
+          <span> - Deploy at {packageJson.datetimedeploy}</span>
+        )}
+    </footer>
+  );
 }
 
 export default Footer;
