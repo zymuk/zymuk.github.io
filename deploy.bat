6c73bcf21ebf77bde385d62452fe7c61c26889db
Tran T. Ngoc <ttngoc653@gmail.com>
2024-12-15 00:24:05 +0700

    update title and change configues to deploy to host

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 git log -1 --date=format:"%%Y/%%m/%%d %%T %%z" --format="%%ad">dateTimeLastCommit.txt
 set /p dateTimeLastCommit=<dateTimeLastCommit.txt
 del dateTimeLastCommit.txt
-powershell -command "(gc '%~dp0\package.json') -replace '(\"datetimedeploy\": \")(.{0,})(\",)', '${1}%dateTimeLastCommit%${3}' | Out-File -encoding utf8 '%~dp0\package.json'"
+powershell -command "(gc '%~dp0\package.json') -replace '(\"datetimedeploy\": \")(.{0,})(\",)', '${1}%dateTimeLastCommit%${3}' | Out-File -encoding ASCII '%~dp0\package.json'"
 yarn predeploy
 yarn deploy
