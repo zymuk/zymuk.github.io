9661b22895cc8242f650fc7a04cccb08a48e1b05
Tran T. Ngoc <ttngoc653@gmail.com>
2024-12-18 03:14:27 +0700

    add social for footer

--- src/frontend/components/footer/Footer.jsx
+++ src/frontend/components/footer/Footer.jsx
@@ -4,6 +4,17 @@ import packageJson from "../../../../package.json";
 function Footer() {
   return (
     <footer className="footer">
+      <div className="social">
+        <a href="mailto:ttngoc653@gmail.com">
+          <i className="bi bi-envelope"></i>
+        </a>
+        <a href="https://www.facebook.com/">
+          <i className="bi bi-facebook"></i>
+        </a>
+        <a href="http://github.com/zymuk">
+          <i className="bi bi-github"></i>
+        </a>
+      </div>
       <p className="copyright">
         <span>Copyright 2024 by Zymuk Tran. All Rights Reserved.</span>
         {packageJson.datetimedeploy !== undefined &&
