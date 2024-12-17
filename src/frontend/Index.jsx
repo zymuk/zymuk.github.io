8759a1f8c04f4b113cf4db5b7f6c46f66f8d284e
Tran T. Ngoc <ttngoc653@gmail.com>
2024-12-18 03:16:46 +0700

    merge header and process only in 1 jsx header

--- src/frontend/Index.jsx
+++ src/frontend/Index.jsx
@@ -1,29 +1,15 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Tools from "./pages/tools/Tools";
-import HomeHeader from "./components/header/HomeHeader";
-import { useEffect, useState } from "react";
 
 function FrontEndIndex() {
-  const [isHomePage, setIsHomePage] = useState(false);
-
-  const location = useLocation();
-
-  useEffect(() => {
-    const regexHomePage = new RegExp(
-      "([hpts]{4,}[:/]{0,}[a-zA-Z0-9.:]{1,}[/]{0,}([#]{1,}[a-zA-Z0-9]{0,}){0,}[/]{0,}){1,}$"
-    );
-    const checkHonePage = regexHomePage.test(window.location.href);
-    setIsHomePage(checkHonePage);
-  }, [location]);
-
   return (
     <div className="container">
-      {(isHomePage && <HomeHeader />) || <Header />}
+      <Header />
       <div className="frontend-content">
         <Routes>
           <Route exact path="/*" element={<Home />} />
--- src/frontend/components/header/Header.jsx
+++ src/frontend/components/header/Header.jsx
@@ -1,19 +1,67 @@
+import { useEffect, useState } from "react";
 import "./header.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const [isHomePage, setIsHomePage] = useState(false);
+  const [isHideMenuMobile, setIsHideMenuMobile] = useState(true);
+  const [menuSelected, setMenuSelected] = useState("");
+
+  const linkToSection = ["Home", "Information", "Contact", "Apps"];
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const regexHomePage = new RegExp(
+      "([hpts]{4,}[:/]{0,}[a-zA-Z0-9.:]{1,}[/]{0,}([#]{1,}[a-zA-Z0-9]{0,}){0,}[/]{0,}){1,}$"
+    );
+    const checkHonePage = regexHomePage.test(window.location.href);
+    setIsHomePage(checkHonePage);
+  }, [location]);
+
+  useEffect(() => {
+    let menuIconElement = document.querySelector(".header #menu-icon");
+    if (window.getComputedStyle(menuIconElement).visibility === "hidden")
+      setIsHideMenuMobile(false);
+  }, []);
+
+  const handleClickMenuIcon = () => {
+    const newValue = isHideMenuMobile === false;
+    setIsHideMenuMobile(newValue);
+  };
+
+  const handleMenu = (menuName) => {
+    setMenuSelected(menuName);
+  };
+
   return (
     <header className="header">
-      <Link to="/" className="logo">
-        Zimuk
-        <span>Tran</span>
-      </Link>
-      <i className="bi bi-list" id="menu-icon" />
-      <nav className="navbar">
-        <Link to="/" className="active">
-          Home
-        </Link>
-        <Link to="/tools">Tools</Link>
+      <a href={window.location.origin} className="logo">
+        Zymuk
+        <span> Page</span>
+      </a>
+      <i
+        className={isHideMenuMobile ? "bi bi-list" : "bi bi-x"}
+        id="menu-icon"
+        onClick={handleClickMenuIcon}
+      />
+      <nav
+        className="navbar"
+        style={{ display: isHideMenuMobile ? "none" : "block" }}
+      >
+        {(isHomePage &&
+          linkToSection.map((item) => {
+            return (
+              <a
+                key={item}
+                href={"#" + item}
+                onClick={() => handleMenu(item)}
+                className={menuSelected === item ? "active" : null}
+              >
+                {item}
+              </a>
+            );
+          })) || <Link to="/tools">Tools</Link>}
       </nav>
     </header>
   );
