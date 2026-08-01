bd7dbf11e88a7eca9a2eed1101d69d08cc5cbe4b
zymuk <ttngoc653@gmail.com>
2026-08-01 21:18:03 +0700

    feat: add JSON-LD person schema and code splitting

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import Site from "./site/Site";
-import Admin from "./admin/Admin";
 import "./index.css";
+
+const Site = lazy(() => import("./site/Site"));
+const Admin = lazy(() => import("./admin/Admin"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        <Route path="/*" element={<Site />} />
-        <Route path="/admin/*" element={<Admin />} />
-      </Routes>
+      <Suspense fallback={<div className="app-loading">Loading...</div>}>
+        <Routes>
+          <Route path="/*" element={<Site />} />
+          <Route path="/admin/*" element={<Admin />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   </React.StrictMode>
 );
